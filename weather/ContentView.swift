4365f06f04548d3c88bc5bc80abae2ebbf941ced
ridhoafwani <zikrurridhoafwani@gmail.com>
2024-04-06 13:46:52 +0700

    Initial Commit

---
+++ weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather
+//
+//  Created by Muhammad Zikrurridho Afwani on 06/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Muhammad Zikrurridho Afwani on 06/04/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
